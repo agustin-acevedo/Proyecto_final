c5d046b6a443e0ca9331ccbce712958915a6db0e
Agustin Acevedo <ac.nahuel.agustin@gmail.com>
2025-03-11 11:36:48 -0300

    Nuevo modelo probado

--- clasificadores/XGBoost.py
+++ clasificadores/XGBoost.py
@@ -44,7 +44,20 @@ y_test = np.loadtxt("./datos/caracteristicas" + str(num_caract) + "selector" + s
 le = pickle.load(open('./modelos/le.sav', 'rb'))
 
 # Configurar y entrenar el modelo XGBoost
-model = xgb.XGBClassifier(objective='multi:softmax', num_class=10, eval_metric="mlogloss", n_estimators=200, learning_rate=0.1, max_depth=6)
+model = xgb.XGBClassifier(
+    objective='multi:softmax', 
+    num_class= 10, 
+    eval_metric="mlogloss", 
+    n_estimators=200, 
+    learning_rate=0.1, 
+    max_depth=8,
+    subsample = 0.8,
+    colsample_bytree= 0.8,
+    random_state= 42,
+    n_jobs = -1
+    )
+
+
 print ("FASE DE ENTRENAMIENTO")
 t_inicio_entrenamiento = time.time()
 model.fit(X_train, y_train)
