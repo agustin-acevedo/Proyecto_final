64fad3aceaec12a35ef605efb0dc3de708881fe9
Agustin Acevedo <ac.nahuel.agustin@gmail.com>
2025-05-15 20:08:57 -0300

    cambios para desplegue

--- app.py
+++ app.py
@@ -296,5 +296,7 @@ def imagenes(filename):
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
